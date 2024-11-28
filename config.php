86e21fd6e4135ee883535f49999498b75bf4b99c
nesshy <ux3301@gmail.com>
2024-11-28 03:29:04 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('THREADS_DIR', __DIR__ . '/threads');  // Directory for storing thread files
+define('UPLOADS_DIR', __DIR__ . '/uploads');  // Directory for storing uploaded images
+define('BASE_URL', 'http://enterurlhere.com');  // Base URL of your imageboard
+?>
